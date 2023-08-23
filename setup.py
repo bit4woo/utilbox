f592a86e8543fa2b37c466a1333d6d59706fea0b
bit4woo <bit4woo@163.com>
2023-08-23 13:49:10 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,24 @@
 # coding: utf-8
-# coding: utf-8
 from setuptools import setup, find_packages
 
 setup(
-    name='utilbox',
-    version='0.1',
+    name="utilbox",
+    version="0.1.0",
+    author="bit4woo",
+    author_email="your.email@example.com",
+    description="common used functions for me",
+    long_description="common used functions for me",
+    long_description_content_type="text/markdown",
+    url="https://github.com/bit4woo/utilbox",
     packages=find_packages(),
-    install_requires=[
-        "BeautifulSoup",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
+    install_requires=[
+        "requests",
+        "beautifulsoup4",
+    ],
+    python_requires=">=3.8",
 )
