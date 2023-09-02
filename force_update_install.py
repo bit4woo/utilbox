db76694a3f41f4beec676858376cdbc42ea33f68
bit4woo <bit4woo@163.com>
2023-09-02 13:24:23 +0800

    Update force_update_install.py

--- force_update_install.py
+++ force_update_install.py
@@ -1,15 +1,16 @@
 import subprocess
+
 import pip
 
 try:
-    subprocess.run("python3.8 -m pip install --force-reinstall git+https://github.com/bit4woo/utilbox.git")
+    subprocess.run("python3.8 -m pip install --force-reinstall git+https://github.com/bit4woo/utilbox.git", shell=True)
 except:
     try:
-        subprocess.run("python3.8 -m pip uninstall utilbox")
+        subprocess.run("python3.8 -m pip uninstall utilbox", shell=True)
     except:
         pip.main(["uninstall", "-y", "utilbox"])  # Use -y flag to automatically confirm uninstallation
 
     try:
-        subprocess.run("python3.8 -m pip install git+https://github.com/bit4woo/utilbox.git")
+        subprocess.run("python3.8 -m pip install git+https://github.com/bit4woo/utilbox.git", shell=True)
     except:
         pip.main(["install", "git+https://github.com/bit4woo/utilbox.git"])
